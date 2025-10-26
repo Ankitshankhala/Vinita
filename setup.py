b34d76637efdb967d2bc6e7bee8a1c8739592195
Ankitshankhala <132757004+Ankitshankhala@users.noreply.github.com>
2025-10-26 15:52:50 +0530

    Vin's Project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vinita",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Ankiit Shankhala",
+    author_email="ankitshankhala2112@gmail.com",
+    description="Vinita project package",
+)
